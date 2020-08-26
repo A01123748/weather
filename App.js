45816297b6d00060731513d1e60edd8d363aefb1
Eliseo Fuentes <eliseo.fuentes@anglobal.com>
2020-08-26 16:30:42 -0500

    Include Platform in App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+import { Platform, StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./navigation/AppNavigator";
